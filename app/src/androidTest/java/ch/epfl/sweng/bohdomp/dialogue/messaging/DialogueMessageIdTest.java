806428011975996137047a95d7756812f17d9d27
VladH <vlad.haprian@epfl.ch>
2014-11-08 13:31:36 +0100

    WhiteSpaces added

--- app/src/androidTest/java/ch/epfl/sweng/bohdomp/dialogue/messaging/DialogueMessageIdTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bohdomp/dialogue/messaging/DialogueMessageIdTest.java
@@ -12,18 +12,20 @@ public class DialogueMessageIdTest extends AndroidTestCase {
 
     public void testGetNewDialogueMessageId() {
         messageId = DialogueMessage.DialogueMessageId.getNewDialogueMessageId();
+
         assertTrue(messageId != null);
     }
 
     public void testGetLong() {
         messageId = DialogueMessage.DialogueMessageId.getNewDialogueMessageId();
+
         assertTrue(messageId != null);
         assertTrue(messageId.getLong() > 0);
     }
 
     public void testFromLong() {
-
         messageId=DialogueMessage.DialogueMessageId.fromLong(id);
+
         assertTrue(messageId != null);
         assertTrue(messageId.getLong() == id);
     }
@@ -33,9 +35,11 @@ public class DialogueMessageIdTest extends AndroidTestCase {
         DialogueMessage.DialogueMessageId messageId1;
         DialogueMessage.DialogueMessageId messageId2;
         DialogueMessage.DialogueMessageId messageId3;
+
         messageId1=DialogueMessage.DialogueMessageId.fromLong(id);
         messageId2=DialogueMessage.DialogueMessageId.fromLong(id);
         messageId3=DialogueMessage.DialogueMessageId.fromLong(id+1);
+
         assertTrue(messageId3.compareTo(messageId1) > 0);
         assertTrue(messageId2.compareTo(messageId1) == 0);
     }
@@ -45,9 +49,11 @@ public class DialogueMessageIdTest extends AndroidTestCase {
         DialogueMessage.DialogueMessageId messageId1;
         DialogueMessage.DialogueMessageId messageId2;
         DialogueMessage.DialogueMessageId messageId3;
+
         messageId1=DialogueMessage.DialogueMessageId.fromLong(id);
         messageId2=DialogueMessage.DialogueMessageId.fromLong(id);
         messageId3=DialogueMessage.DialogueMessageId.fromLong(id+1);
+
         assertTrue(messageId1.equals(messageId2));
         assertFalse(messageId1.equals(messageId3));
         assertFalse(messageId1.equals(null));
