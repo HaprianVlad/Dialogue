012ba988cb1a1ceb35369ac71d650254d90fd74d
Jakob Odersky <jodersky@gmail.com>
2014-12-11 11:09:57 +0100

    fix missing null checks

--- app/src/main/java/ch/epfl/sweng/bohdomp/dialogue/crypto/openpgp/generation/KeyRingPair.java
+++ app/src/main/java/ch/epfl/sweng/bohdomp/dialogue/crypto/openpgp/generation/KeyRingPair.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.bohdomp.dialogue.crypto.openpgp.generation;
 
 import ch.epfl.sweng.bohdomp.dialogue.crypto.openpgp.PublicKeyRing;
 import ch.epfl.sweng.bohdomp.dialogue.crypto.openpgp.SecretKeyRing;
+import ch.epfl.sweng.bohdomp.dialogue.utils.Contract;
 
 /**
  * Regroups a public and secret key ring
@@ -12,6 +13,9 @@ public class KeyRingPair {
     private final SecretKeyRing mSecret;
 
     public KeyRingPair(PublicKeyRing pub, SecretKeyRing sec) {
+        Contract.throwIfArgNull(pub, "pub");
+        Contract.throwIfArgNull(sec, "sec");
+
         this.mPublic = pub;
         this.mSecret = sec;
     }
